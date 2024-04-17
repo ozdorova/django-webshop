f051075504d0a2ea79052c9395453704fc515b38
Mikhail <mbold1911@yandex.ru>
2024-04-17 17:56:10 +0300

    add cart views

--- myshop/urls.py
+++ myshop/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop'))
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 
