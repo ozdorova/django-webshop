6de26ac0921c23c42520291d941d89db2139fac2
Mikhail <mbold1911@yandex.ru>
2024-04-17 14:30:34 +0300

    add views

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -19,6 +20,9 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -29,8 +33,8 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=0, decimal_places=2)
-    avalable = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
@@ -43,6 +47,11 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
