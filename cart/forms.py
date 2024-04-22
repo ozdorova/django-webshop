e170aa899e3bd80ac0aa5822b8f3ead67cf8fbf0
Mikhail <mbold1911@yandex.ru>
2024-04-22 14:57:34 +0300

    fix

--- cart/forms.py
+++ cart/forms.py
@@ -6,9 +6,11 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
     """Форма позволяющая выбирать количество товаров в корзине"""
-    
-    #выбор количества товаров от 1 до 20
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
-    
+
+    # выбор количества товаров от 1 до 20
+    quantity = forms.TypedChoiceField(
+        choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='Количество')
+
     # поле для указания, нужно ли перезаписывать количество товаров в корзине, скрыто от пользователя
-    override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+    override = forms.BooleanField(
+        required=False, initial=False, widget=forms.HiddenInput)
